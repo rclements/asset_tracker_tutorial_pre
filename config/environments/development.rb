6522e1b8cc906a1e00d7bd4563b0893a012552ee
Adam Dill <altrux@gmail.com>
2010-09-02 12:50:44 -0500

    [ARCH] fixed deprecation notice logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,10 +9,11 @@ AssetTrackerTutorial::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
-  # Show full error reports and disable caching
+  # Show full error reports and disable caching and write deprecation notices to log
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
+  config.active_support.deprecation        = :log
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
