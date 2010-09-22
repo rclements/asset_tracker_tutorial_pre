4f7b22eed1bb7949826b1b61852843336b5e5513
James Cook <james@isotope11.com>
2010-09-22 13:06:12 -0500

    Fix failing tests in project & client specs. Add coverage in ticket spec.

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe Client do
+  let(:client) { Client.new }
+  subject{ client }
+
   it "fails validation with no name" do
-    Client.new.should have(1).errors_on(:name)
+    should have(1).errors_on(:name)
   end
 
   it "fails validation with no status" do
-    Client.new.should have(1).errors_on(:status)
+    should have(1).errors_on(:status)
   end
 
-  context "with an existing client with the same name" do
+  describe "with an existing client with the same name" do
     before(:each) do
-      client = Client.create(:name => 'test', :status => 'test')
+      Client.create(:name => "test", :status => "Good")
     end
 
+    let(:client_dup) { Client.create(:name => "test", :status => "Good") }
+
+    subject{ client_dup }
     it "fails validation with error on name" do
-      Client.new(:name => 'test', :status => 'test').should have(1).errors_on(:name)
+      should have(1).errors_on(:name)
     end
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,4 +4,8 @@ describe Ticket do
   it "fails validation with no project_id" do
     Ticket.new.should have(1).errors_on(:project_id)
   end
+
+  it "fails validation with no name" do
+    Ticket.new.should have(1).errors_on(:name)
+  end
 end
