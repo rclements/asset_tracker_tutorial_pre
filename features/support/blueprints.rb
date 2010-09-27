c023c2593b1434806ab94c66b909c0719112268b
Nicholas Fine <nicholas.fine@gmail.com>
2010-09-27 14:36:06 -0500

    Added blueprint for user model

--- features/support/blueprints.rb
+++ features/support/blueprints.rb
@@ -10,20 +10,30 @@ end
 
 Project.blueprint do
   client { Client.make }
-  name { Faker::Company.name }
-  guid { UUID.generate }
+  name   { Faker::Company.name }
+  guid   { UUID.generate }
 end
 
 Ticket.blueprint do
-  project { Project.make }
-  name { 'some name' }
-  priority 'high'
+  project     { Project.make }
+  name        { 'some name' }
+  priority    'high'
   description { Faker::Company.name + "'s ticket" }
-  guid { UUID.generate }
+  guid        { UUID.generate }
+end
+
+User.blueprint do
+  first_name     { Faker::Name.first_name }
+  last_name      { Faker::Name.last_name  }
+  middle_initial { ('A'..'Z').to_a.rand   }
+  email          { Faker::Internet.email  }
+  password_confirmation 'password'
+  password 'password'
 end
 
 WorkUnit.blueprint do
-  ticket { Ticket.make }
+  ticket      { Ticket.make }
+  user        { User.make }
   description { Faker::Company.name + "'s work unit" }
-  guid { UUID.generate }
+  guid        { UUID.generate }
 end
