8b486b9a113b1d4c51ed69f4f7a2e7e6eeb39b1f
Ben Holley <benolee@gmail.com>
2010-09-28 16:39:47 -0500

    Added validations to address and contact models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,9 @@ class Contact < ActiveRecord::Base
   belongs_to :client
 
   has_one :address, :as => :addressable
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email_address
 end
+
