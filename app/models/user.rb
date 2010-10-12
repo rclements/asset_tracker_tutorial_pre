18bcaba61f1a2fc79e2baa8592dcd23284102512
Ben Holley <ben@isotope11.com>
2010-10-12 10:35:58 -0500

    Removed user registerable from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :lockable
 
   acts_as_authorization_subject :association_name => :roles
 
