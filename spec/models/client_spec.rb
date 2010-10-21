ba8fea1362835470f119e4db627ba5d8d05de8dd
Ben Holley <ben@isotope11.com>
2010-10-21 13:59:36 -0500

    [RSPEC] Added shoulda tests for Client and Ticket models

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,22 +1,41 @@
 require 'spec_helper'
 
 describe Client do
-  let(:client) { Client.new }
-  subject{ client }
 
-  it "should allow comments" do
-    subject.respond_to?(:comments).should be true
+  it "should have many projects" do
+    should have_many(:projects)
   end
 
-  it "fails validation with no name" do
-    should have(1).errors_on(:name)
+  it "should have many tickets through projects" do
+    should have_many(:tickets).through(:projects)
   end
 
-  it "fails validation with no status" do
-    should have(1).errors_on(:status)
+  it "should validate presence of name" do
+    should validate_presence_of(:name)
   end
 
-  describe "with an existing client with the same name" do
+  it "should validate presence of status" do
+    should validate_presence_of(:status)
+  end
+
+  context 'when newly created' do
+    let(:client) { Client.new }
+    subject{ client }
+
+    it "should allow comments" do
+      subject.respond_to?(:comments).should be true
+    end
+
+    it "fails validation with no name" do
+      should have(1).errors_on(:name)
+    end
+
+    it "fails validation with no status" do
+      should have(1).errors_on(:status)
+    end
+  end
+
+  context 'with an existing client with the same name' do
     before(:each) do
       Client.create(:name => "test", :status => "Good")
     end
