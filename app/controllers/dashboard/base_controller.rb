486b8966d6a412442a3fc17b870ae88148ffd48f
bradley price <bradley@potsdam.com>
2010-10-22 14:47:41 -0500

    [DASHBOARD] added jquery accordion functionality, still needs to be tested

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,7 +1,9 @@
 class Dashboard::BaseController < ApplicationController
-  
+
   def index
-    
+    @work_units = WorkUnit.joins(:user).where('user_id = ? AND scheduled_at > ?', current_user, 8.days.ago)
+    @days = @work_units.map{ |wu| wu.scheduled_at.strftime("%A, %B %d, %Y") }.uniq
+
   end
-  
+
 end
