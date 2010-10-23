3bb32a5155fea52c9855cfda57a9ee1606d217c5
Bradley Price <bradley@isotope11.com>
2010-10-22 19:32:29 -0500

    [DASHBOARD] is a user hasn't entered time the day before, they should be presented with a modal that tells them.

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,6 +1,11 @@
 class Dashboard::BaseController < ApplicationController
 
   def index
+
+    if current_user.has_no_time_for(1.day.ago)
+      @no_time = "true"
+    end
+
     @work_units = WorkUnit.joins(:user).where('user_id = ? AND scheduled_at > ?', current_user, 8.days.ago)
     @days = @work_units.map{ |wu| wu.scheduled_at.strftime("%A, %B %d, %Y") }.uniq
 
