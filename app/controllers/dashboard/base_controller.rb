6ec571c09bfe180956da1099163a9f94662067de
bradley price <bradley@potsdam.com>
2010-10-25 09:43:54 -0500

    [DASHBOARD] changed the modal a little

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -2,8 +2,10 @@ class Dashboard::BaseController < ApplicationController
 
   def index
 
-    unless current_user.time_for(1.day.ago)
+    if current_user.time_for(1.day.ago).empty?
       @no_time = "true"
+      @title = "Laird says..."
+      @message = "You have not entered any time for yesterday. You're fired!"
     end
 
     @work_units = WorkUnit.joins(:user).where('user_id = ? AND scheduled_at > ?', current_user, 8.days.ago).reverse
