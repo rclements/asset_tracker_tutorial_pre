d9f795b2a2240e2716c6d32cdebd404dea3ec509
Bradley Price <bradley@isotope11.com>
2010-10-26 15:25:44 -0500

    [DASHBOARD] added combo boxes to add_work_unit partial

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,4 +1,5 @@
 class Dashboard::BaseController < ApplicationController
+  respond_to :html, :json, :js
 
   def index
     if current_user.work_units_for_day(1.day.ago).empty?
@@ -7,6 +8,20 @@ class Dashboard::BaseController < ApplicationController
 
     @work_units = WorkUnit.joins(:user).where('user_id = ? AND scheduled_at > ?', current_user, 8.days.ago).reverse
     @days = @work_units.map{ |wu| wu.scheduled_at.strftime("%A, %B %d, %Y") }.uniq
+    @clients = Client.all
+
+    @projects ||= []
+    @tickets ||= []
+  end
+
+  def client
+    @projects = Project.find(:all, :conditions => ['client_id = ?', params[:id]])
+    respond_with @projects
+  end
+
+  def project
+    @tickets = Ticket.find(:all, :conditions => ['project_id = ?', params[:id]])
+    respond_with @tickets
   end
 
 private
