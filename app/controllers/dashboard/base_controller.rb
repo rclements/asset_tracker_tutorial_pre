d820715fdccb56e912a898547f064df9f7d5f60b
Bradley Price <bradley@isotope11.com>
2010-10-26 10:30:55 -0500

    [DASHBOARD] created method show_message so that the modal can be called a little easier.

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,16 +1,19 @@
 class Dashboard::BaseController < ApplicationController
 
   def index
-
     if current_user.work_units_for_day(1.day.ago).empty?
-      @no_time = "true"
-      @title = "Laird says..."
-      @message = "You have not entered any time for yesterday. You're fired!"
+      show_message("Laird says...", "You have not entered any time for yesterday. You're fired!")
     end
 
     @work_units = WorkUnit.joins(:user).where('user_id = ? AND scheduled_at > ?', current_user, 8.days.ago).reverse
     @days = @work_units.map{ |wu| wu.scheduled_at.strftime("%A, %B %d, %Y") }.uniq
+  end
 
+private
+  def show_message(title, message)
+    @show_message = "true"
+    @title = title
+    @message = message
   end
 
 end
