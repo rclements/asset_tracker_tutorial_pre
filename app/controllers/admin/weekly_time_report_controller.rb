1968ef4c5371cfffd5784cf4f86b0ba3b4463bcc
Adam Dill <altrux@gmail.com>
2010-11-01 17:07:46 -0500

    [WEEKLY TIME REPORT] overhauled weekly time report controller

--- app/controllers/admin/weekly_time_report_controller.rb
+++ app/controllers/admin/weekly_time_report_controller.rb
@@ -1,30 +1,13 @@
 class Admin::WeeklyTimeReportController < ApplicationController
-
+  
   def index
-    monday = Time.now.beginning_of_week.strftime("%F")
-    redirect_to('/admin/weekly_time_report/' + monday)
+    redirect_to('/admin/weekly_time_report/' + Date.today.beginning_of_week.strftime("%F"))
   end
 
   def show
-    @beg_date = Time.parse(params[:id])
-
-    if !@beg_date.monday?
-      monday = @beg_date.beginning_of_week.strftime("%F")
-      redirect_to('/admin/weekly_time_report/' + monday)
-    end
-
+    redirect_unless_monday('/admin/weekly_time_report/', params[:id])
+    # FIXME: users should have role 'developer', we don't want to show non-entered time for non developers
     @users = User.unlocked
-    # users should have role 'developer': we don't want to show non-entered time for 'admin' or 'client user accounts'
-
-    @weekdays = []
-    (0..6).each do |day|
-      @weekdays << @beg_date.beginning_of_week + day.days
-    end
-
-    @previous_week = (@beg_date.beginning_of_week - 1.week).strftime("%F")
-    @beginning_of_week = @beg_date.beginning_of_week
-    @end_of_week = @beg_date.end_of_week
-    @next_week = (@beg_date.next_week).strftime("%F")
   end
 
 end
