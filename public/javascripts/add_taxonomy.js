fb589a975c3805eb63b4724715c32b40c455d113
Adam Dill <altrux@gmail.com>
2010-11-11 20:22:07 -0600

    [TAXONOMY] copied the work unit js for tickets

--- public/javascripts/add_taxonomy.js
+++ public/javascripts/add_taxonomy.js
@@ -1,3 +1,6 @@
+// ---------- Schedule Modal
+
+// Setup the dialog
 $("#schedule_modal").dialog({ 
   modal: true,
   autoOpen: false,
@@ -5,11 +8,13 @@ $("#schedule_modal").dialog({
   height: 500
 });
 
+// open the dialog
 $('#schedule_modal_link').click(function() {
   $("#schedule_modal").dialog('open');
   return false;
 });
 
+// setup datepicker inside the dialog
 $("#scheduled_at").datepicker( {
   onSelect: function(dateText, inst) {
     $("#work_unit_scheduled_at").val(dateText);
@@ -19,6 +24,44 @@ $("#scheduled_at").datepicker( {
   dateFormat: 'yy-mm-dd'
 });
 
+
+// ---------- Work Unit Creation
+
+// when client is changed, populate the projects
+$("#work_unit_client_id").change(function(){
+  var me = $("#work_unit_project_id");
+  me.children().remove();
+  me.append( new Option("Select a project","") )
+  $("#work_unit_ticket_id").children().remove();
+  $("#work_unit_ticket_id").append( new Option("Select a ticket",""))
+  if(this.value != "") {
+    $.get("/dashboard/client", { id: this.value }, function(data){
+      $.each(data, function(){
+        $.each(this, function(k, v){
+          me.append( new Option(v.name, v.id) )
+        });
+      });
+    }, "json");
+  }
+});
+
+// when project is changed, populate the tickets
+$("#work_unit_project_id").change(function(){
+  var me = $("#work_unit_ticket_id")
+  me.children().remove();
+  me.append( new Option("Select a ticket","") )
+  if(this.value != "") {
+    $.get("/dashboard/project", { id: this.value }, function(data){
+      $.each(data, function(){
+        $.each(this, function(k, v){
+          me.append( new Option(v.name, v.id) )
+        });
+      });
+    }, "json");
+  }
+});
+
+// AJAX work unit creation
 $("#new_work_unit").submit(function() {
   var me = $(this);
   $.ajax(
@@ -53,6 +96,7 @@ $("#new_work_unit").submit(function() {
   return false;
 });
 
+// Error handling on failed AJAX work unit creation
 $("#work_unit_errors").dialog( {
   autoOpen: false,
   hide: true,
@@ -72,8 +116,11 @@ $("#work_unit_errors").dialog( {
   }
 });
 
-$("#work_unit_client_id").change(function(){
-  var me = $("#work_unit_project_id");
+// ---------- Ticket Creation
+
+// when client is changed, populate the projects
+$("#ticket_client_id").change(function(){
+  var me = $("#ticket_project_id");
   me.children().remove();
   me.append( new Option("Select a project","") )
   $("#work_unit_ticket_id").children().remove();
@@ -89,8 +136,9 @@ $("#work_unit_client_id").change(function(){
   }
 });
 
-$("#work_unit_project_id").change(function(){
-  var me = $("#work_unit_ticket_id")
+// when project is changed, populate the tickets
+$("#ticket_project_id").change(function(){
+  var me = $("#ticket_ticket_id")
   me.children().remove();
   me.append( new Option("Select a ticket","") )
   if(this.value != "") {
@@ -101,5 +149,54 @@ $("#work_unit_project_id").change(function(){
         });
       });
     }, "json");
+  }
+});
+
+// AJAX ticket creation
+$("#new_ticket").submit(function() {
+  var me = $(this);
+  $.ajax(
+  {
+    async: true,
+    global: false,
+    url: me.attr("action"),
+    type: "POST",
+    dataType: "json",
+    data: {"ticket[project_id]": me.find("#ticket_project_id").val(),
+      "ticket[name]": me.find("#ticket_name").val(),
+      "ticket[description]": me.find("#ticket_description").val() },
+    success: function(result)
+    {
+      var json = $.parseJSON( result.responseText )
+      me.trigger("reset");
+      me.effect("highlight");
+    },
+    error: function(result)
+    {
+      var json = $.parseJSON( result.responseText )
+      $("#ticket_errors").data('errors', json);
+      $("#ticket_errors").dialog('open');
+    }
+  });
+  return false;
+});
+
+// Error handling on failed AJAX ticket creation
+$("#ticket_errors").dialog( {
+  autoOpen: false,
+  hide: true,
+  title: "Error",
+  modal: true,
+  draggable: false,
+  open: function() {
+    var dialog = $("#ticket_errors")
+    dialog.html("")
+    $.each( dialog.data('errors'), function() {
+      dialog.append("<p>" + this + "</p>")
+    });
+  },
+  close: function() {
+    var dialog = $("#ticket_errors")
+    dialog.html("");
   }
 });
