8b7cc52f42438560005d3ff05f9c609fb278eda2
Nicholas Fine <nicholas.fine@gmail.com>
2010-11-30 17:00:40 -0600

    'i' before 'e' except after 'c'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101129214537) do
+ActiveRecord::Schema.define(:version => 20101130225328) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
