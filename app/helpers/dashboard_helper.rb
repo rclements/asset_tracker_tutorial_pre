7019ec1ee057a708177331d4c0e7964c4d2ceec8
Adam Dill <altrux@gmail.com>
2010-11-30 10:13:17 -0600

    [HELPER] removed unused helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,8 +1,2 @@
 module DashboardHelper
-  def work_unit_sum_and_count_for(date)
-    date = date.to_time.utc
-    bucket = WorkUnit.for_user(current_user).scheduled_between(date, date + 1.day)
-
-    count, sum = bucket.count, bucket.sum(:hours)
-  end
 end
