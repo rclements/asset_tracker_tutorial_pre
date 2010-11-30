0e8e6d8671be9ccdc6e075d44a63e78922097d4f
Ben Holley <ben@isotope11.com>
2010-11-30 15:46:48 -0600

    [DASHBOARD] fix problem with passing in datetime parameter on creating work units

--- app/controllers/work_units_controller.rb
+++ app/controllers/work_units_controller.rb
@@ -17,7 +17,9 @@ class WorkUnitsController < ApplicationController
     _params.delete :project_id
     @work_unit = WorkUnit.new(_params)
     @work_unit.user = current_user
-    @work_unit.scheduled_at ||= Time.zone.now
+    @work_unit.scheduled_at = DateTime.strptime(_params[:scheduled_at], "%m/%d/%Y")
+    debugger
+    puts "foo"
   end
 
   def load_work_unit
