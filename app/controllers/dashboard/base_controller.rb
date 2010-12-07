0d1cf33b68a1250cf6a01403d8b50069036640c6
Ben Holley <ben@isotope11.com>
2010-12-07 09:05:44 -0600

    [DASHBOARD] removed many unnecessary lines of code

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,25 +1,7 @@
 class Dashboard::BaseController < ApplicationController
   before_filter :get_calendar_details, :only => [:index, :calendar]
-  before_filter :load_all_projects, :only => [:index]
-  before_filter :load_all_tickets, :only => [:index]
   respond_to :html, :json, :js
 
-  def load_all_projects
-    # TODO need a better/cleaner way to do the following conditional for all the methods in this controller
-    if admin?
-      @projects = Project.all
-    else
-      @projects = Project.all.select {|p| p.allows_access?(current_user) }
-    end
-  end
-
-  def load_all_tickets
-    if admin?
-      @tickets = Ticket.all
-    else
-      @tickets = Ticket.all.select {|t| t.allows_access?(current_user) }
-    end
-  end
   public
 
   def index
@@ -28,28 +10,23 @@ class Dashboard::BaseController < ApplicationController
         :body => t(:enter_time_for_previous_day)}
     end
 
-    if admin?
-      @clients = Client.all
-    else
-      @clients = Client.all.select {|c| c.allows_access?(current_user) }
-    end
-    @tickets  ||= []
+    @clients = Client.for_user current_user
+    @projects = Project.for_user current_user
+    @tickets = Ticket.for_user current_user
   end
 
   def client
-    if admin?
-      @projects = Project.find(:all, :conditions => ['client_id = ?', params[:id]])
-    else
-      @projects = Project.find(:all, :conditions => ['client_id = ?', params[:id]]).select {|p| p.allows_access?(current_user) }
+    @projects = Project.find(:all, :conditions => ['client_id = ?', params[:id]])
+    unless admin?
+      @projects = @projects.select {|p| p.allows_access?(current_user)}
     end
     respond_with @projects
   end
 
   def project
-    if admin?
-      @tickets = Ticket.find(:all, :conditions => ['project_id = ?', params[:id]])
-    else
-      @tickets = Ticket.find(:all, :conditions => ['project_id = ?', params[:id]]).select {|t| t.allows_access?(current_user) }
+    @tickets = Ticket.find(:all, :conditions => ['project_id = ?', params[:id]])
+    unless admin?
+      @tickets = @tickets.select {|t| t.allows_access?(current_user) }
     end
     respond_with @tickets
   end
@@ -58,6 +35,7 @@ class Dashboard::BaseController < ApplicationController
   end
 
   private
+
   def get_calendar_details
     if params[:date].present?
       @start_date = Date.parse(params[:date]).beginning_of_week
