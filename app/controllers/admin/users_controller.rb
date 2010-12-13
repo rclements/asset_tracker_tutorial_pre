367aa469a375be76030777f130b7faa1b1d6b078
Ben Holley <ben@isotope11.com>
2010-12-13 16:33:08 -0600

    [USERS] sort users by first name

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < Admin::BaseController
   before_filter :load_new_user_account, :only => [:new, :create]
 
   def index
-    @users = User.all.sort_by {|user| user.first_name}
+    @users = User.all.sort_by {|user| user.first_name.downcase}
   end
 
   def new
